e61ea5cddc4a65c167dae840f4ce1ec2b356ec45
mmoc0910 <mmoc0910@gmail.com>
2025-12-30 11:36:07 +0700

    comment code scroll trigger

--- js/pr_courtly.js
+++ js/pr_courtly.js
@@ -1,78 +1,78 @@
-// use a script tag or an external JS file
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
-  // gsap code here!
+// // use a script tag or an external JS file
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   gsap.registerPlugin(ScrollTrigger);
+//   // gsap code here!
 
-  gsap.utils.toArray(".section-heading").forEach((el) => {
-    gsap.from(el, {
-      opacity: 0,
-      rotation: -80,
-      transformOrigin: "bottom left",
-      duration: 0.5,
-      scrollTrigger: {
-        trigger: el,
-        start: "top 50%",
-      },
-    });
-  });
+//   gsap.utils.toArray(".section-heading").forEach((el) => {
+//     gsap.from(el, {
+//       opacity: 0,
+//       rotation: -80,
+//       transformOrigin: "bottom left",
+//       duration: 0.5,
+//       scrollTrigger: {
+//         trigger: el,
+//         start: "top 50%",
+//       },
+//     });
+//   });
 
-  gsap
-    .timeline({ scrollTrigger: { trigger: ".project-intro" } })
-    .from(".project-role", { opacity: 0, x: -20, duration: 1 }, 0)
-    .from(".project-title", { opacity: 0, x: -20, duration: 1 }, 0.2)
-    .from(".project-link", { opacity: 0, x: -20, duration: 1 }, 0.4)
-    .from(".project-right", { opacity: 0, x: 20, duration: 1 }, 0.4);
+//   gsap
+//     .timeline({ scrollTrigger: { trigger: ".project-intro" } })
+//     .from(".project-role", { opacity: 0, x: -20, duration: 1 }, 0)
+//     .from(".project-title", { opacity: 0, x: -20, duration: 1 }, 0.2)
+//     .from(".project-link", { opacity: 0, x: -20, duration: 1 }, 0.4)
+//     .from(".project-right", { opacity: 0, x: 20, duration: 1 }, 0.4);
 
-  gsap.from(".tools-items", {
-    opacity: 0,
-    x: 100,
-    duration: 1,
-    scrollTrigger: { trigger: ".tools-items", start: "top 60%" },
-  });
+//   gsap.from(".tools-items", {
+//     opacity: 0,
+//     x: 100,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".tools-items", start: "top 60%" },
+//   });
 
-  gsap.from(".design-focus-card", {
-    opacity: 0,
-    y: 50,
-    duration: 1,
-    scrollTrigger: { trigger: ".design-focus-card", start: "top 80%" },
-  });
+//   gsap.from(".design-focus-card", {
+//     opacity: 0,
+//     y: 50,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".design-focus-card", start: "top 80%" },
+//   });
 
-  gsap.from(".target-discipline-header", {
-    opacity: 0,
-    y: 60,
-    duration: 1,
-    scrollTrigger: { trigger: ".target-discipline-header", start: "top 80%" },
-  });
+//   gsap.from(".target-discipline-header", {
+//     opacity: 0,
+//     y: 60,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".target-discipline-header", start: "top 80%" },
+//   });
 
-  gsap.from(".discipline-cards", {
-    opacity: 0,
-    x: 50,
-    duration: 1,
-    scrollTrigger: { trigger: ".discipline-cards", top: "top 60%" },
-  });
+//   gsap.from(".discipline-cards", {
+//     opacity: 0,
+//     x: 50,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".discipline-cards", top: "top 60%" },
+//   });
 
-  gsap.from(".about-slide-wp", {
-    duration: 1,
-    opacity: 0,
-    y: 100,
-    scrollTrigger: { trigger: ".about-slide-wp", start: "top 60%" },
-  });
-  gsap.from(".intro-text", {
-    duration: 1,
-    opacity: 0,
-    x: 100,
-    scrollTrigger: { trigger: ".intro-text", start: "top 60%" },
-  });
-  gsap.from(".tablet-frame", {
-    opacity: 0,
-    y: 40,
-    duration: 1,
-    scrollTrigger: { trigger: ".tablet-frame", start: "top 60%" },
-  });
-  gsap.from(".projects-wrapper", {
-    opacity: 0,
-    y: 40,
-    duration: 1,
-    scrollTrigger: { trigger: ".projects-wrapper", start: "top 60%" },
-  });
-});
+//   gsap.from(".about-slide-wp", {
+//     duration: 1,
+//     opacity: 0,
+//     y: 100,
+//     scrollTrigger: { trigger: ".about-slide-wp", start: "top 60%" },
+//   });
+//   gsap.from(".intro-text", {
+//     duration: 1,
+//     opacity: 0,
+//     x: 100,
+//     scrollTrigger: { trigger: ".intro-text", start: "top 60%" },
+//   });
+//   gsap.from(".tablet-frame", {
+//     opacity: 0,
+//     y: 40,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".tablet-frame", start: "top 60%" },
+//   });
+//   gsap.from(".projects-wrapper", {
+//     opacity: 0,
+//     y: 40,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".projects-wrapper", start: "top 60%" },
+//   });
+// });
--- js/pr_whispurr.js
+++ js/pr_whispurr.js
@@ -1,56 +1,56 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollTrigger);
-  // gsap code here!
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   gsap.registerPlugin(ScrollTrigger);
+//   // gsap code here!
 
-  gsap.utils.toArray(".section-heading").forEach((el) => {
-    gsap.from(el, {
-      opacity: 0,
-      rotation: -80,
-      transformOrigin: "bottom left",
-      duration: 0.5,
-      scrollTrigger: {
-        trigger: el,
-        start: "top 50%",
-      },
-    });
-  });
+//   gsap.utils.toArray(".section-heading").forEach((el) => {
+//     gsap.from(el, {
+//       opacity: 0,
+//       rotation: -80,
+//       transformOrigin: "bottom left",
+//       duration: 0.5,
+//       scrollTrigger: {
+//         trigger: el,
+//         start: "top 50%",
+//       },
+//     });
+//   });
 
-  gsap
-    .timeline({ scrollTrigger: { trigger: ".project-intro" } })
-    .from(".project-role", { opacity: 0, x: -20, duration: 1 }, 0)
-    .from(".project-title", { opacity: 0, x: -20, duration: 1 }, 0.2)
-    .from(".project-link", { opacity: 0, x: -20, duration: 1 }, 0.4)
-    .from(".project-right", { opacity: 0, x: 20, duration: 1 }, 0.4);
+//   gsap
+//     .timeline({ scrollTrigger: { trigger: ".project-intro" } })
+//     .from(".project-role", { opacity: 0, x: -20, duration: 1 }, 0)
+//     .from(".project-title", { opacity: 0, x: -20, duration: 1 }, 0.2)
+//     .from(".project-link", { opacity: 0, x: -20, duration: 1 }, 0.4)
+//     .from(".project-right", { opacity: 0, x: 20, duration: 1 }, 0.4);
 
-  gsap.from(".tools-items", {
-    opacity: 0,
-    x: 100,
-    duration: 1,
-    scrollTrigger: { trigger: ".tools-items", start: "top bottom" },
-  });
+//   gsap.from(".tools-items", {
+//     opacity: 0,
+//     x: 100,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".tools-items", start: "top bottom" },
+//   });
 
-  gsap.from(".design-focus-header", {
-    opacity: 0,
-    y: -50,
-    duration: 1,
-    scrollTrigger: { trigger: ".design-focus-header", start: "top 60%" },
-  });
-  gsap.from(".tag-section", {
-    opacity: 0,
-    x: -50,
-    duration: 1,
-    scrollTrigger: { trigger: ".tag-section", start: "top 60%" },
-  });
+//   gsap.from(".design-focus-header", {
+//     opacity: 0,
+//     y: -50,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".design-focus-header", start: "top 60%" },
+//   });
+//   gsap.from(".tag-section", {
+//     opacity: 0,
+//     x: -50,
+//     duration: 1,
+//     scrollTrigger: { trigger: ".tag-section", start: "top 60%" },
+//   });
 
-  gsap
-    .timeline({
-      scrollTrigger: { trigger: ".section-break", start: "top 60%" },
-    })
-    .from(".break.brleft", { opacity: 0, y: 30, duration: 1 }, 0)
-    .from(".break.brright", { opacity: 0, y: 30, duration: 1 }, 0.5);
+//   gsap
+//     .timeline({
+//       scrollTrigger: { trigger: ".section-break", start: "top 60%" },
+//     })
+//     .from(".break.brleft", { opacity: 0, y: 30, duration: 1 }, 0)
+//     .from(".break.brright", { opacity: 0, y: 30, duration: 1 }, 0.5);
 
-  gsap
-    .timeline({ scrollTrigger: { trigger: ".section-rs", start: "top 60%" } })
-    .from(".image-boxleft", { opacity: 0, y: 50, duration: 1 }, 0)
-    .from(".image-boxright", { opacity: 0, x: 50, duration: 1 }, 0.2);
-});
+//   gsap
+//     .timeline({ scrollTrigger: { trigger: ".section-rs", start: "top 60%" } })
+//     .from(".image-boxleft", { opacity: 0, y: 50, duration: 1 }, 0)
+//     .from(".image-boxright", { opacity: 0, x: 50, duration: 1 }, 0.2);
+// });
